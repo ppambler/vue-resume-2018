20692fd09bc4e1e01cbc565163a48e44524b782e
Ambler <allinlbj@gmail.com>
2018-09-01 16:21:21 +0800

    add 注册登录保存登出功能

--- src/js/app.js
+++ src/js/app.js
@@ -11,14 +11,55 @@ let app = new Vue({
             jobTitle: '前端工程师',
             phone: '138111111111',
             email: 'example@example.com'
+        },
+        login: {
+            email: '',
+            password: ''
+        },
+        signUp: {
+            email: '',
+            password: ''
         }
     },
     methods: {
         onEdit(key, value) {
             this.resume[key] = value
         },
+        onLogin(e) {
+            AV.User.logIn(this.login.email, this.login.password).then(function (loggedInUser) {
+                console.log(loggedInUser);
+            }, function (error) {
+                if (error.code === 211) {
+                    alert('邮箱不存在！')
+                } else if (error.code === 210) {
+                    alert('邮箱和密码不匹配！')
+                }
+            });
+        },
+        onLogout(e) {
+            AV.User.logOut();
+            alert('注销成功!')
+            window.location.reload()
+        },
+        onSignUp(e) {
+            // 新建 AVUser 对象实例
+            const user = new AV.User();
+            // 设置用户名
+            user.setUsername(this.signUp.email);
+            // 设置密码
+            user.setPassword(this.signUp.password);
+            // 设置邮箱
+            user.setEmail(this.signUp.email);
+            user.signUp().then(function (loggedInUser) {
+                console.log(loggedInUser);
+                alert('注册成功！')
+                window.location.reload()
+            }, function (error) {
+            });
+        },
         onClickSave(){
             let currentUser = AV.User.current()
+            console.log(currentUser);
             if (!currentUser) {
                 this.loginVisible = true
             } else {
@@ -26,7 +67,15 @@ let app = new Vue({
             }
         },
         saveResume() {
-
+            let {id} = AV.User.current()
+            // 第一个参数是 className，第二个参数是 objectId
+            var user = AV.Object.createWithoutData('User', id);
+            // 修改属性
+            user.set('resume', this.resume);
+            // 保存到云端
+            user.save().then(()=>{
+                alert('保存成功！')
+            });
         }
     }
 });
