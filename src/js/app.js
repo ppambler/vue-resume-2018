76aefb01b06eff81a56c7b2e21625fe0183b4ba3
Ambler <allinlbj@gmail.com>
2018-09-04 18:49:43 +0800

    add projects

--- src/js/app.js
+++ src/js/app.js
@@ -21,6 +21,10 @@ let app = new Vue({
                 {name: '请填写技能名称', description: '请填写技能描述'},
                 {name: '请填写技能名称', description: '请填写技能描述'},
                 {name: '请填写技能名称', description: '请填写技能描述'}
+            ],
+            projects: [
+                {name: '请填写项目名称', link: 'http://...', keywords: '请填写关键词', description: '请详细描述'},
+                {name: '请填写项目名称', link: 'http://...', keywords: '请填写关键词', description: '请详细描述'},
             ]
         },
         login: {
@@ -134,6 +138,14 @@ let app = new Vue({
         },
         removeSkill(index) {
             this.resume.skills.splice(index, 1)
+        },
+        addProject() {
+            this.resume.projects.push(
+                {name: '请填写项目名称', link: 'http://...', keywords: '请填写关键词', description: '请详细描述'},
+            )
+        },
+        removeProject(index) {
+            this.resume.projects.splice(index, 1)
         }
     }
 });
